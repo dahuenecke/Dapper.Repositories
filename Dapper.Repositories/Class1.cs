172abda58b6e935af0579be002e9c4f3284bf735
dahuenecke <dhuenecke@outlook.com>
2019-03-01 15:56:38 -0600

    adding dependencies

--- Dapper.Repositories/Class1.cs
+++ Dapper.Repositories/Class1.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dapper;
+using MySql.Data.MySqlClient;
+using Oracle.ManagedDataAccess.Client;
+
 
 namespace Dapper.Repositories
 {
     public class Class1
     {
+
     }
 }
